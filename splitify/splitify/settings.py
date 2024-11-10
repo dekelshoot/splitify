b77e9cadb2fdbb2038cdb06825c28bd4afe6719b
dekelshoot <69307490+dekelshoot@users.noreply.github.com>
2024-11-10 21:39:40 +0000

    vs

--- splitify/splitify/settings.py
+++ splitify/splitify/settings.py
@@ -82,6 +82,10 @@ WSGI_APPLICATION = 'splitify.wsgi.application'
 
 CORS_ALLOW_ALL_ORIGINS = True
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://localhost:8000',
+    'http://127.0.0.1:8000',
+]
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
