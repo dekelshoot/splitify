f0892328962acae7c2bca085a9bb9ab682719f26
dekelshoot <69307490+dekelshoot@users.noreply.github.com>
2024-11-12 05:27:41 +0000

    4

--- splitify/setup.sh
+++ splitify/setup.sh
@@ -1,10 +1,10 @@
 # Update the package list
 echo "Updating package list..."
-sudo apt update
-
+#sudo apt update
+apt update
 # Install FFmpeg
 echo "Installing FFmpeg..."
-sudo apt install -y ffmpeg
-
+#sudo apt install -y ffmpeg
+apt install -y ffmpeg
 echo "Installing Python dependencies from requirements.txt..."
 pip install -r requirements.txt
